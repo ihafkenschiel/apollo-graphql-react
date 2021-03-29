ba1521c212040bb310d04c11ef16a45d069d4ade
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2021-03-29 16:52:35 -0300

    Installed an Express server with GraphQL

---
+++ server/server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+ 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    allCourses: [Course]
+    Course(id: Int!): Course
+  },
+  type Course {
+      id: Int
+      title: String
+      author: String
+      description: String
+      topic: String
+      url: String
+  }
+`);
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+ 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
