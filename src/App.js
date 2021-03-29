f38cbed3a7e58312466a94dc1b9ec2a907672fc6
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2021-03-29 15:16:47 -0300

    Starting frontend with React and Apollo to access GraphQL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Courses from './Courses';
+
+const apolloClient = new ApolloClient({
+  uri: ''
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div>
+        <Courses />
+      </div>
+    </ApolloProvider>
   );
 }
 
